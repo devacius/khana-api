4cbd3c28179f37cc5c4676031da25a198938e5a3
Devacius <devaciusaboveall789456@gmail.com>
2023-10-26 21:57:48 +0530

    after changing the app.js

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
