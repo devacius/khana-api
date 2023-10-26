e2035bec6555d7514d19eec57b7c677e434782db
Deepansh Gupta <81462832+devacius@users.noreply.github.com>
2023-10-26 23:27:17 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -67,4 +67,7 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
 });
 
-app.listen(3000);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
